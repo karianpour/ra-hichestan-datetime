5934bf2ea7e8bc40ab02386477e60f3c34384d90
Kayvan Arianpour <karianpour@gmail.com>
2019-09-04 17:37:57 +0430

    package update and removing moment js.

--- src/lib/JalaliField.js
+++ src/lib/JalaliField.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import moment from 'moment-jalaali';
+import jalaali from 'jalaali-js';
 import PropTypes from 'prop-types';
 
 const JalaliField = ({ source, record = {}, showTime }) => <span>{formatDate(record[source], showTime)}</span>;
@@ -17,14 +17,39 @@ JalaliField.defaultProps = {
 };
 
 
-const date_time_format = 'jYYYY/jMM/jDD HH:mm';
-const date_format = 'jYYYY/jMM/jDD';
+const DATE_SEPERATOR =  '/';// this is arabic date seperator ' ؍' but it is right to left glyph and as the numbers are left to right there will be caret position problem
+const MIDDLE_SEPERATOR =  '\xa0';
+const TIME_SEPERATOR =  ':';
 
 function formatDate(date, showTime){
   if(!date) return date;
-  const jDate = moment(date);
+  if(!(date instanceof Date)){
+    date = new Date(date);
+  }
 
-  return jDate.isValid() ? '\u200E' + jDate.format(showTime ? date_time_format : date_format) : "##";
+  if(date.toString() === 'Invalid Date') return '##';
+
+  return `\u200E${formatJalaliDate(date)}${showTime? MIDDLE_SEPERATOR + formatTime(date):''}`;
+}
+
+function formatJalaliDate(date){
+  if(!date) return '';
+  if(!(date instanceof Date)){
+    date = new Date(date);
+  }
+  const jDate = jalaali.toJalaali(date);
+  const day = jDate.jd.toString();
+  const month = jDate.jm.toString();
+  const year = jDate.jy.toString();
+  return `${'0000'.substring(year.length)}${year}${DATE_SEPERATOR}${'00'.substring(day.length)}${day}${DATE_SEPERATOR}${'00'.substring(month.length)}${month}`;
+}
+
+function formatTime(date){
+  if(!date) return '';
+  let hours = date.getHours().toString()
+  let minutes = date.getMinutes().toString()
+
+  return `${'00'.substring(hours.length)}${hours}${TIME_SEPERATOR}${'00'.substring(minutes.length)}${minutes}`;
 }
 
 export default JalaliField;
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,4 @@
 import JalaliInput from "./JalaliInput";
 import JalaliField from "./JalaliField";
-export { JalaliInput, JalaliField };
+import JalaliTimeInput from "./JalaliTimeInput";
+export { JalaliInput, JalaliField, JalaliTimeInput };
